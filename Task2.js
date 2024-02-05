fbeaf51274f7242a97980583b31539648654a6fd
Ofallbrains <ofallbrains@gmail.com>
2024-02-05 15:41:51 +0200

    Project

--- Task2.js
+++ Task2.js
@@ -2,9 +2,10 @@
 const colors = ['red', 'green', 'blue', 'yellow', 'orange'];
 console.log(colors.join(","))
 // Q2
-console.log(colors.splice(2, 2, "purple", "pink"))
+// console.log(colors.splice(2, 2, "purple", "pink"))
 // Q3
 console.log(colors.copyWithin(0, 1, 3))
 // 04
 
 // 05
+console.log(colors.find((color)=> color.startsWith('b')))
