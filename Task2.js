70960dd05537b0ea3ebd4b3686d3bab274a8f6ba
Ofallbrains <ofallbrains@gmail.com>
2024-02-05 15:34:50 +0200

    Project

---
+++ Task2.js
@@ -0,0 +1,10 @@
+// Q1
+const colors = ['red', 'green', 'blue', 'yellow', 'orange'];
+console.log(colors.join(","))
+// Q2
+console.log(colors.splice(2, 2, "purple", "pink"))
+// Q3
+console.log(colors.copyWithin(0, 1, 3))
+// 04
+
+// 05
