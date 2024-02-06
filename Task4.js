c03371e50ba76fe8513ec9dbe044499e2c089112
Ofallbrains <ofallbrains@gmail.com>
2024-02-06 12:32:49 +0200

    ok

---
+++ Task4.js
@@ -0,0 +1,7 @@
+// create task manager where the user can add, remove and mark task as completed
+// a. Add task to task list
+// b. Remove task from task list
+// c. Sort task alphabetically
+// d. Display the number of incomplete task
+
+const = []
