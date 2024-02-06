112f56c63f446b410068b4011b00ebb6f0c57c52
Ofallbrains <ofallbrains@gmail.com>
2024-02-06 14:42:07 +0200

    JavaScript

---
+++ Task3.js
@@ -0,0 +1,27 @@
+const items = [
+    { name: 'Bike', price: 100 },
+    { name: 'TV', price: 200 },
+    { name: 'Album', price: 10 },
+    { name: 'Book', price: 5 },
+    { name: 'Phone', price: 500 },
+    { name: 'Computer', price: 1000 },
+];
+// 1. Filter and show the product that will be bought when you don't have much money (Cheap one)
+// 2. Filter and show the product that will be expensive in the array
+// 3. Calculate the full price of all products combined
+// 4. Calculate the full price of all products combined and remove products that are under $10
+// 5. Filter and show the product that will be start with letter b at first postion
+
+// q1
+const result = items.filter((item) => item.price <= 10)
+result.map((item) =>
+console.log(item.name))
+// console.log(result)
+// q2
+const expensiveProduct = items.filter(item => item.price >= 500)
+console.log(expensiveProduct)
+
+// q3.
+const total = items.reduce((a, b) => a + b)
+console.log(total)
+
